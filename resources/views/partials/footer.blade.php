1e316a57f85ec105fd959402f4306ac2688dd2d2
Guido-95 <guidocarna1995@gmail.com>
2021-07-13 15:53:52 +0200

    main

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+
+    <link rel="stylesheet" href="{{ asset("css/provaprova.css") }}">
+ 
+    <footer>
+        <div class="container">
+            <h3>footer</h3>
+        </div>
+       
+    </footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+
+
+<header>
+    <div class="container">
+        <ul class="list-header">
+            <li>Characters</li>
+            <li>Characters</li>
+            <li>Characters</li>
+            <li>Characters</li>
+            <li>Characters</li>
+        </ul>
+    </div>
+    
+</header>
